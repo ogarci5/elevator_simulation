d16dad5356a25112118ec9479c36b80d1fef65cd
Oliver Garcia <ogarci5@gmail.com>
2019-11-07 18:13:03 -0600

    Rename copied files

--- spec/elevator_simulation_spec.rb
+++ spec/elevator_simulation_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Panomosity do
+RSpec.describe ElevatorSimulation do
   it 'has a version number' do
-    expect(Panomosity::VERSION).not_to be nil
+    expect(ElevatorSimulation::VERSION).not_to be nil
   end
 
   it 'does something useful' do
